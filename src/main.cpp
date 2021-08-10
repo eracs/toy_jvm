8b57d3deca18fbf8d085cb50f91555f0f50b067a
eracs <eracs@qq.com>
2021-08-10 11:55:05 +0800

    日志调整

--- src/main.cpp
+++ src/main.cpp
@@ -11,11 +11,11 @@ void initLogger()
 {
     auto console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
     console_sink->set_level(spdlog::level::debug);
-    console_sink->set_pattern("[%Y-%m-%d %H:%M:%S] [%n] [%^--%L--%$] [thread %t] %v");
+    console_sink->set_pattern("[%Y-%m-%d %H:%M:%S.%e] [%n] [%^--%L--%$] [thread %t] %v");
 
     auto file_sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>("logs/toy_jvm.log", true);
     file_sink->set_level(spdlog::level::debug);
-    file_sink->set_pattern("[%Y-%m-%d %H:%M:%S] [%n] [%^--%L--%$] [thread %t] %v");
+    file_sink->set_pattern("[%Y-%m-%d %H:%M:%S.%e] [%n] [%^--%L--%$] [thread %t] %v");
 
     std::vector<spdlog::sink_ptr> sinks{console_sink, file_sink};
     auto logger = std::make_shared<spdlog::logger>("Logger", sinks.begin(), sinks.end());
