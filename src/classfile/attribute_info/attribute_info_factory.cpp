d0ec71e004e618b806b226eacdd6a3854f42a117
eracs <eracs@qq.com>
2021-08-25 20:14:17 +0800

    add log

--- src/classfile/attribute_info/attribute_info_factory.cpp
+++ src/classfile/attribute_info/attribute_info_factory.cpp
@@ -3,10 +3,11 @@
 #include "ConstantValue_attribute.h"
 #include "Deprecated_attribute.h"
 #include "Synthetic_attribute.h"
-
+#include "spdlog/spdlog.h"
 
 std::shared_ptr<Attribute_Info> createAttributeInfo(const unsigned char *data, size_t &current_ptr, const size_t &dataSize, int &status, ConstantPool *constantPool)
 {
+    spdlog::get("Logger")->debug("Attribute:read attribute");
     std::shared_ptr<Attribute_Info> empty(nullptr);
     return empty;
 }
