6aac4e24a4de461f71cf5691877252cc1352930a
eracs <eracs@qq.com>
2021-09-11 21:22:41 +0800

    attribute factory

--- src/classfile/attribute_info/attribute_info_factory.cpp
+++ src/classfile/attribute_info/attribute_info_factory.cpp
@@ -9,10 +9,21 @@
 #include "LineNumberTable_attribute.h"
 #include "StackMapTable_attribute.h"
 #include "InnerClasses_attribute.h"
+#include "../class_reader.h"
+#include "../constant_pool/CONSTANT_Utf8_info.h"
 
 std::shared_ptr<Attribute_Info> createAttributeInfo(const unsigned char *data, size_t &current_ptr, const size_t &dataSize, int &status, std::shared_ptr<ConstantPool> constantPool)
 {
     spdlog::get("Logger")->debug("Attribute:read attribute");
+
+    u2 attribute_name_index = readNextU2(data, current_ptr, dataSize, status);
+    u4 attribute_length = readNextU4(data, current_ptr, dataSize, status);
+    std::shared_ptr<ConstantInfo> utf8Info = constantPool->getItems()[attribute_name_index];
+    if (utf8Info->getTag() == 1)
+    {
+        std::string attributeName = std::static_pointer_cast<CONSTANT_Utf8_info>(utf8Info)->getUtf8Str();
+    }
+    current_ptr += attribute_length;
     std::shared_ptr<Attribute_Info> empty(nullptr);
     return empty;
 }
