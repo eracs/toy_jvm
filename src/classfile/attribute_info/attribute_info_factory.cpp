1ff8d6d2bdb9d4d28ad225ab294ddfa05e79279b
eracs <eracs@qq.com>
2021-09-12 21:08:23 +0800

    attribute factory update

--- src/classfile/attribute_info/attribute_info_factory.cpp
+++ src/classfile/attribute_info/attribute_info_factory.cpp
@@ -18,10 +18,16 @@ std::shared_ptr<Attribute_Info> createAttributeInfo(const unsigned char *data, s
 
     u2 attribute_name_index = readNextU2(data, current_ptr, dataSize, status);
     u4 attribute_length = readNextU4(data, current_ptr, dataSize, status);
-    std::shared_ptr<ConstantInfo> utf8Info = constantPool->getItems()[attribute_name_index];
+    auto &cpItems = constantPool->getItems();
+    std::shared_ptr<ConstantInfo> utf8Info = cpItems[attribute_name_index];
     if (utf8Info->getTag() == 1)
     {
         std::string attributeName = std::static_pointer_cast<CONSTANT_Utf8_info>(utf8Info)->getUtf8Str();
+        if (attributeName == "ConstantValue")
+        {
+            u2 constantvalue_index = readNextU2(data, current_ptr, dataSize, status);
+            return std::make_shared<ConstantValue_attribute>(attributeName, attribute_length, cpItems[constantvalue_index]);
+        }
     }
     current_ptr += attribute_length;
     std::shared_ptr<Attribute_Info> empty(nullptr);
